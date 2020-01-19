1ab6dc9f921241f00806a80832d3e2ac4b257622
Sam <hello@samsedgeman.co.uk>
2020-01-19 17:19:27 +0000

    Wired up Task Edit Button and added css styles

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster-ufk0a.mongodb.net/task_manager?retryWrites=true&w=majority"
+os.environ["MONGO_DB"] = "task_manager"
